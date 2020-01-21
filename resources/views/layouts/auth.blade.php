8f66f06cfd5e2aee70fe7d6410bb91ffe127cbc8
avinash-glocify <avinash.glocify@gmail.com>
2020-01-21 12:55:37 +0530

    project setup

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <title>{{ config('app.name', 'Laravel') }}</title>
+
+  <!-- Fonts -->
+  <link rel="dns-prefetch" href="//fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+  <link rel="stylesheet" href="{{ asset('vendors//datatables.net-bs4/dataTables.bootstrap4.css') }}">
+
+  <!-- Styles -->
+  <link href="{{ asset('vendors/base/vendor.bundle.base.css') }}" rel="stylesheet">
+  <link href="{{ asset('vendors/mdi/css/materialdesignicons.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+</head>
+<body>
+  <div id="app">
+    <div class="container-scroller">
+      @include('layouts.header')
+      <div class="container-fluid page-body-wrapper">
+        @include('layouts.sidebar')
+        <div class="main-panel">
+          @yield('content')
+          @include('layouts.footer')
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+<script src="{{ asset('js/app.js') }}" defer></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<!-- <script src="/vendors/base/vendor.bundle.base.js"></script> -->
+<!-- Plugin js for this page-->
+<script src="{{ asset('vendors/chart.js/Chart.min.js') }}"></script>
+<script src="{{ asset('vendors/datatables.net/jquery.dataTables.js') }}"></script>
+<script src="{{ asset('vendors/datatables.net-bs4/dataTables.bootstrap4.js') }}"></script>
+<!-- End plugin js for this page-->
+<!-- inject:js -->
+<script src="{{ asset('js/theme/off-canvas.js') }}"></script>
+<script src="{{ asset('js/theme/hoverable-collapse.js') }}"></script>
+<script src="{{ asset('js/theme/template.js') }}"></script>
+<!-- endinject -->
+<!-- Custom js for this page-->
+<script src="{{ asset('js/theme/dashboard.js') }}"></script>
+<script src="{{ asset('js/theme/data-table.js') }}"></script>
+<script src="{{ asset('js/theme/jquery.dataTables.js') }}"></script>
+<script src="{{ asset('js/theme/dataTables.bootstrap4.js') }}"></script>
+<!-- End custom js for this page-->
+</html>
---
+++ resources/views/test/sidebar.blade.php
@@ -0,0 +1,63 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+  <ul class="nav">
+    <li class="nav-item">
+      <a class="nav-link" href="/dashboard">
+        <i class="mdi mdi-home menu-icon"></i>
+        <span class="menu-title">Dashboard</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+        <i class="mdi mdi-circle-outline menu-icon"></i>
+        <span class="menu-title">UI Elements</span>
+        <i class="menu-arrow"></i>
+      </a>
+      <div class="collapse" id="ui-basic">
+        <ul class="nav flex-column sub-menu">
+          <li class="nav-item"> <a class="nav-link" href="#">Buttons</a></li>
+          <li class="nav-item"> <a class="nav-link" href="#">Typography</a></li>
+        </ul>
+      </div>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">
+        <i class="mdi mdi-view-headline menu-icon"></i>
+        <span class="menu-title">Form elements</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">
+        <i class="mdi mdi-chart-pie menu-icon"></i>
+        <span class="menu-title">Charts</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">
+        <i class="mdi mdi-grid-large menu-icon"></i>
+        <span class="menu-title">Tables</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">
+        <i class="mdi mdi-emoticon menu-icon"></i>
+        <span class="menu-title">Icons</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
+        <i class="mdi mdi-account menu-icon"></i>
+        <span class="menu-title">User Pages</span>
+        <i class="menu-arrow"></i>
+      </a>
+      <div class="collapse" id="auth">
+        <ul class="nav flex-column sub-menu">
+          <li class="nav-item"> <a class="nav-link" href="#"> Login </a></li>
+          <li class="nav-item"> <a class="nav-link" href="#"> Login 2 </a></li>
+          <li class="nav-item"> <a class="nav-link" href="#"> Register </a></li>
+          <li class="nav-item"> <a class="nav-link" href="#"> Register 2 </a></li>
+          <li class="nav-item"> <a class="nav-link" href="#"> Lockscreen </a></li>
+        </ul>
+      </div>
+    </li>
+  </ul>
+</nav>
