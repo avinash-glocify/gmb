cab535d56e7f4ad1bee059e99dc4f955620fa649
avinash-glocify <avinash.glocify@gmail.com>
2020-01-28 13:53:15 +0530

    seeder updated

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,7 +11,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(CategoryTableSeeder::class);
-        $this->call(BussinessTypeTableSeeder::class);
+        // $this->call(CategoryTableSeeder::class);
+        // $this->call(BussinessTypeTableSeeder::class);
     }
 }
